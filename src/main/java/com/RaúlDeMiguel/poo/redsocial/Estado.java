abb996b947ab03f6b0d0d9405b591161c787efba
rraaull7 <rraaulldemiguel@gmail.com>
2025-12-11 10:03:13 +0100

    hola2

--- src/main/java/com/RaúlDeMiguel/poo/redsocial/Estado.java
+++ src/main/java/com/RaúlDeMiguel/poo/redsocial/Estado.java
@@ -2,4 +2,9 @@ package com.RaúlDeMiguel.poo.redsocial;
 
 public enum Estado {
 
+	
+	
+	ACTIVO, NO_ACTIVO, BLOQUEADO
+	
+	
 }
--- src/main/java/com/RaúlDeMiguel/poo/redsocial/PerfilRedSocial.java
+++ src/main/java/com/RaúlDeMiguel/poo/redsocial/PerfilRedSocial.java
@@ -1,246 +1,156 @@
 package com.RaúlDeMiguel.poo.redsocial;
 
-public class PerfilRedSocial {
+import java.util.ArrayList;
 
-	private String nombreUsuario;
-	private String nombreVisible;
+
+public class PerfilRedSocial {
+	
+	private String usuario;
+	private String nombre;
 	private String biografia;
 	private String pais;
 	private int seguidores;
-	private int publicaciones;
+	
+	
+	private boolean verificada;
+	private Estado estado;
+	private ArrayList<Publicacion> publicaciones;
+ 
+	
+	
+	
+	
+	
+	public PerfilRedSocial(String usuario, String nombre, String biografia, String pais, int seguidores,
+			boolean verificada, Estado estado) {
+		super();
+		
+		
+		this.usuario = usuario;
+		this.nombre = nombre;
+		this.biografia = biografia;
+		this.pais = pais;
+		this.seguidores = seguidores;
+		this.verificada = verificada;
+		this.estado = estado;
+		this.publicaciones = new ArrayList<Publicacion>();
+	}
+ 
 	public PerfilRedSocial() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-
-
-
-
-
-
-
-
-	private boolean verificado;
-	private Estado estado;
-	
-	
-	
-	
-	
-	
-	
-	
-	public String getNombreUsuario() {
-		return nombreUsuario;
+ 
+ 
+	public String getUsuario() {
+		return usuario;
 	}
-
-
-
-
-
-
-
-
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
+ 
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
 	}
-
-
-
-
-
-
-
-
-	public String getNombreVisible() {
-		return nombreVisible;
+ 
+	public String getNombre() {
+		return nombre;
 	}
-
-
-
-
-
-
-
-
-	public void setNombreVisible(String nombreVisible) {
-		this.nombreVisible = nombreVisible;
+ 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-
-
-
-
-
-
-
-
+ 
 	public String getBiografia() {
 		return biografia;
 	}
-
-
-
-
-
-
-
-
+ 
 	public void setBiografia(String biografia) {
 		this.biografia = biografia;
 	}
-
-
-
-
-
-
-
-
+ 
 	public String getPais() {
 		return pais;
 	}
-
-
-
-
-
-
-
-
+ 
 	public void setPais(String pais) {
 		this.pais = pais;
 	}
-
-
-
-
-
-
-
-
+ 
 	public int getSeguidores() {
 		return seguidores;
 	}
-
-
-
-
-
-
-
-
+ 
 	public void setSeguidores(int seguidores) {
 		this.seguidores = seguidores;
 	}
-
-
-
-
-
-
-
-
-	public int getPublicaciones() {
+ 
+	public int getTotalPublicaciones() {
+		return publicaciones.size();
+	}
+ 
+ 
+	public ArrayList<Publicacion> getPublicaciones() {
 		return publicaciones;
 	}
-
-
-
-
-
-
-
-
-	public void setPublicaciones(int publicaciones) {
+ 
+	public void setPublicaciones(ArrayList<Publicacion> publicaciones) {
 		this.publicaciones = publicaciones;
 	}
-
-
-
-
-
-
-
-
-	public boolean isVerificado() {
-		return verificado;
+ 
+	public boolean isVerificada() {
+		return verificada;
 	}
-
-
-
-
-
-
-
-
-	public void setVerificado(boolean verificado) {
-		this.verificado = verificado;
+ 
+	public void setVerificada(boolean verificada) {
+		this.verificada = verificada;
 	}
-
-
-
-
-
-
-
-
+ 
 	public Estado getEstado() {
 		return estado;
 	}
-
-
-
-
-
-
-
-
+ 
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-
-
-
-
-
-public void mostrarInformacion() {
-	System.out.println(this.nombreUsuario);
+ 
 	
 	
-}
-
-
-public void añadirSeguidores(int seguidores) {
-	this.seguidores +=seguidores;
 	
-	
-
-}
-
-public void estadoPerfil(Estado estado) {
-this.estado=estado;
-	
-}
-
-
-
-public boolean activo(boolean verificado) {
-	if(this.verificado) {
-		System.out.println("NO ESTA ACTIVO");
-	}else {
-		System.out.println("ESTA ACTIVO");
+	public void mostrarInformacion() {
+ 
+		System.out.println(this.nombre);
 	}
-	return this.verificado;
-}
-
-
-
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		
+ 
+	public void añadirSeguidores(int seguidores) {
+		this.seguidores += seguidores;
+ 
 	}
-
+ 
+	public void cambiarEstadoPerfil(Estado estado) {
+		this.estado = estado;
+	}
+ 
+	public boolean activo(boolean verificada) {
+		if (this.verificada) {
+			System.out.println("no esta activo");
+		} else {
+			System.out.println("esta activo");
+		}
+		return this.verificada;
+	}
+ 
+	public void crearPublicacion(String tituloPub) {
+		Publicacion p = new Publicacion();
+		p.setTexto(tituloPub);
+		this.publicaciones.add(p);
+	}
+ 
+	
+	
+	
+	public void mostrarPublicaciones() {
+		for (Publicacion publicacion : publicaciones) {
+			System.out.println(publicacion);
+		}
+	}
 }
+
+
